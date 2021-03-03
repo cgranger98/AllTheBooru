088eb44720a110c53c301fefefc6e5ed91d782d0
Xertal <chris_ciber.2000@hotmail.cl>
2021-03-03 19:29:50 -0300

    Pictures loading for atf

--- src/Components/Front.js
+++ src/Components/Front.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-
+import ReactDOM from 'react-dom';
+import Picture from './Picture.js';
 class Front  extends Component {
 	constructor(){
 		super();
 		this.state = {
 			search:'',
-			site:'atf'
+			site:'atf',
+			picture:null,
+			showPicture: false,
 		}
 		this.search=this.search.bind(this);
 		this.handleSearch=this.handleSearch.bind(this);
 	}
 	search(e){
-		e.preventDefault();
-		console.log(this.state)
+		e.preventDefault();	
+		var json='https://booru.allthefallen.moe/posts.json?tags='+this.state.search+'&is_deleted=false&is_banned=false&limit=2'
+        //fetch(json url).then(response=>{return response.json()}).then(data=>{what to do}).catch(error=>{what to do})
+       fetch(json).then(response=>{
+               return response.json()
+               })
+          .then(data=>{
+               this.setState({
+               'picture':data
+               })
+               console.log(this.state.picture)
+			   this.setState({showPicture:true})
+               })
+           .catch(error=>{
+           console.log(error);    
+       })
+		
+		// ReactDOM.render(<Picture search={this.state.search} site={this.state.site}/>, document.querySelector("#results"))
 	}
 	handleSearch(e){
 		const {value,name} = e.target;
@@ -33,32 +52,38 @@ class Front  extends Component {
 
 	render(){
 		return(
-			<div className="container">
-				<div className="row">
-				<div className="col-4"></div>
-					<div className="col-4">
-						<h1>All The</h1>
-						<h1>Booru</h1>
-						<form onSubmit={this.search}>
-						<div className="col">
-							<input id="radio-atf" name="radio-sel" onChange={this.handleSearch} value="atf" type="radio" defaultChecked></input>
-							<label htmlFor="radio-atf">ATF</label>
-							<input id="radio-e621" name="radio-sel" onChange={this.handleSearch} value="e621" type="radio" className="ms-2"></input>
-							<label htmlFor="radio-e621">E621</label>
-							<div className="row">
-							<input id="inputSearch" name="inputSearch" type="text" onChange={this.handleSearch} className="rounded col" placeholder="Buscar"/>
-							<button className="btn btn-outline-primary text-primary rounded-pill col-3" type="submit">Buscar</button> 
+			<div>
+				<div className="container">
+					<div className="row">
+					<div className="col-4"></div>
+						<div className="col-4">
+							<h1>All The</h1>
+							<h1>Booru</h1>
+							<form onSubmit={this.search}>
+							<div className="col">
+								<input id="radio-atf" name="radio-sel" onChange={this.handleSearch} value="atf" type="radio" defaultChecked></input>
+								<label htmlFor="radio-atf">ATF</label>
+								<input id="radio-e621" name="radio-sel" onChange={this.handleSearch} value="e621" type="radio" className="ms-2"></input>
+								<label htmlFor="radio-e621">E621</label>
+								<div className="row">
+								<input id="inputSearch" name="inputSearch" type="text" onChange={this.handleSearch} className="rounded col" placeholder="Buscar"/>
+								<button className="btn btn-primary rounded-pill col-3" type="submit">Buscar</button> 
+								</div>
 							</div>
-						</div>
-						</form>
+							</form>
+					</div>
+					</div>
+				</div>
+				<div className="container">
+						{this.state.showPicture && 
+						<Picture data={this.state.picture}/>
+						}
 				</div>
 				
-				
-			</div>
+				{/* <div id="results"></div> */}
 			</div>
 		)
 	}
-	
+	 
 }
-
 export default Front;
--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,7 +1,37 @@
 import react,{Component} from 'react';
-
 class Picture extends Component{
 
+    constructor(){
+        super();
+        this.state={
+            'picture': null
+        }
+        
+    }
+    
+    render(){
+      var ext; 
+      const rend = this.props.data.map((data)=>{
+          ext = data.file_ext;
+          console.log(ext);
+          if(ext=='png'||ext=='jpg'||ext=='gif'){
+            return(
+              <div key={data.id} className="col">
+                    <img key={data.id} id={data.id} src={data.file_url} className="card-img-top"></img>
+              </div>
+            )
+          }
+          if(ext=='swf'){console.log('swf cannot display')}
+          if(ext='mp4'||ext=='zip'||ext=='rar'||ext=='webm'){
+            return(
+              <div key={data.id} className="col">
+                    <img key={data.id} id={data.id} loop controls src={data.preview_file_url}
+                    className="card-img-top" ></img>
+              </div>
+            )
+          }
+        })
+        return(<div className="row row-cols-2">{rend}</div>) 
+    }
 }
-
 export default Picture;
